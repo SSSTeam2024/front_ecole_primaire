3d30390391dc6a2b7026a27b383eb6af4f37b52b
SSSTeam2024 <sss.team2024@gmail.com>
2024-11-11 08:55:48 +0100

    push for 11-11

--- src/pages/Absence/index.tsx
+++ src/pages/Absence/index.tsx
@@ -14,6 +14,7 @@ import {
 } from "features/smsSettings/smsSettings";
 
 const Absence = () => {
+  //! add this line just to push it in github !!
   const { data = [] } = useFetchAbsencesQuery();
 
   const { data: AllSmsSettings, isLoading = [] } = useFetchSmsSettingsQuery();
